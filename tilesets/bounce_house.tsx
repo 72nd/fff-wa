143d99bd647cee894f65717e5eb37a3bf7fd525c
72nd <msg@frg72.com>
2021-04-28 15:38:53 +0200

    media files

---
+++ tilesets/bounce_house.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="Bounce House" tilewidth="32" tileheight="32" tilecount="1295" columns="37">
+ <image source="bounce-house.png" width="1204" height="1146"/>
+</tileset>
