db8c56c76c77198cc3ae49ca16689350a1d421ea
72nd <msg@frg72.com>
2021-04-28 16:00:27 +0200

    renamed files

--- tilesets/bounce_house.tsx
+++ tilesets/bounce_house.tsx
@@ -1,4 +1,7 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.4" tiledversion="1.4.3" name="Bounce House" tilewidth="32" tileheight="32" tilecount="1295" columns="37">
- <image source="bounce-house.png" width="1204" height="1146"/>
+<tileset version="1.4" tiledversion="1.4.3" name="Bounce House" tilewidth="32" tileheight="32" tilecount="3190" columns="58">
+ <properties>
+  <property name="collides" type="bool" value="true"/>
+ </properties>
+ <image source="bounce-house.png" width="1882" height="1790"/>
 </tileset>
