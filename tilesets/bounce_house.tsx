d8d1501527bb6f467747f6dcccee02b9d63e30e6
72nd <msg@frg72.com>
2021-04-28 17:50:05 +0200

    removed unused tilesets

--- tilesets/bounce_house.tsx
+++
@@ -1,7 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.4" tiledversion="1.4.3" name="Bounce House" tilewidth="32" tileheight="32" tilecount="3190" columns="58">
- <properties>
-  <property name="collides" type="bool" value="true"/>
- </properties>
- <image source="bounce-house.png" width="1882" height="1790"/>
-</tileset>
--- tilesets/map.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.4" tiledversion="1.4.3" name="Map" tilewidth="32" tileheight="32" tilecount="3190" columns="58">
- <image source="map.png" width="1882" height="1790"/>
-</tileset>
