5ff28bbf2eeb1d0ed5eb0df2154706efde6b3a64
72nd <msg@frg72.com>
2021-05-25 18:42:56 +0200

    lot of new stuff

---
+++ tilesets/big.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.6.0" name="big" tilewidth="32" tileheight="32" tilecount="800" columns="40">
+ <image source="big.png" width="1280" height="640"/>
+</tileset>
