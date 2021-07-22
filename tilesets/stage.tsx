0a021f320a9900bee3ac4bdfdaad31e0c997190a
72nd <msg@frg72.com>
2021-07-22 11:50:32 +0200

    tilesets updated

---
+++ tilesets/stage.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.6.0" name="stage" tilewidth="32" tileheight="32" tilecount="400" columns="20">
+ <image source="stage.png" width="640" height="640"/>
+</tileset>
